7f56c48d3020f4bf3b38806d5f5f06c54bab314c
Oscar Lopez <oscarjoselopez26@gmail.com>
2018-03-15 08:47:10 -0500

    add cocoa pod manager keyboard

--- LearningWords/AppDelegate.swift
+++ LearningWords/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         DataBase.checkAndCreateDatabase(withName: "lwords.db")
+        // Key Board Manager
+        IQKeyboardManager.sharedManager().enable = true
+        
         return true
     }
 
